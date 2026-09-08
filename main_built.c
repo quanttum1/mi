cd883f20f8ec7825df0b2b2b35f1ce076c04e257
jan Luna Wani <janLunaWani@proton.me>
2026-09-08 21:28:35 +0300

    mu

--- main_built.c
+++ main_built.c
@@ -68,20 +68,6 @@ HTML __html(int count, ...)
 #include <stdlib.h>
 #include "da.h"
 
-struct WebTree;
-
-typedef struct {
-    char *name;
-    struct WebTree *content;
-} WebTreeChild;
-
-struct WebTree {
-    char *(*index)();
-    da(WebTreeChild) children;
-};
-
-typedef struct WebTree WebTree;
-
 HTML page_home() {
     return __html(1, strdup("\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x3c\x68\x31\x3e\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x57\x65\x6c\x63\x6f\x6d\x65\x20\x74\x6f\x20\x6d\x79\x20\x77\x65\x62\x73\x69\x74\x65\x21\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x3c\x61\x20\x68\x72\x65\x66\x3d\x22\x61\x22\x3e\x47\x6f\x20\x74\x6f\x20\x70\x61\x67\x65\x20\x41\x3c\x2f\x61\x3e\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x3c\x61\x20\x68\x72\x65\x66\x3d\x22\x62\x22\x3e\x47\x6f\x20\x74\x6f\x20\x70\x61\x67\x65\x20\x42\x3c\x2f\x61\x3e\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x3c\x61\x20\x68\x72\x65\x66\x3d\x22\x62\x2f\x63\x22\x3e\x47\x6f\x20\x74\x6f\x20\x70\x61\x67\x65\x20\x43\x3c\x2f\x61\x3e\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x3c\x2f\x68\x31\x3e\x0a\x20\x20\x20\x20"));
 }
@@ -98,28 +84,49 @@ HTML page_c() {
     return __html(1, strdup("\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x3c\x68\x31\x3e\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x57\x65\x6c\x63\x6f\x6d\x65\x20\x74\x6f\x20\x50\x61\x67\x65\x20\x43\x21\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x3c\x61\x20\x68\x72\x65\x66\x3d\x22\x2f\x22\x3e\x47\x6f\x20\x68\x6f\x6d\x65\x3c\x2f\x61\x3e\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x3c\x2f\x68\x31\x3e\x0a\x20\x20\x20\x20"));
 }
 
-da(WebTree*) webtrees = {0};
+typedef struct {
+    char *path;
+    HTML (*generator)();
+} Page;
 
-void begin_webtree() {
-    da_append(webtrees, malloc(sizeof(WebTree)));
-    *da_last(webtrees) = (WebTree){0};
+typedef da(Page) PageList;
+
+da(PageList) page_lists = {0};
+da(char*) current_paths = {0};
+
+void begin_page_list() {
+    da_append(page_lists, (PageList){0});
+    da_append(current_paths, strdup("/"));
 }
 
 void add_index(HTML (*page)()) {
-    da_last(webtrees)->index = page;
+    char index[] = "index.html";
+    char *new = malloc(strlen(da_last(current_paths)) + sizeof(index));
+    strcpy(new, da_last(current_paths));
+    strcat(new, index);
+    da_append(da_last(page_lists), ((Page){ .path = new, .generator = page }));
 }
 
 void add_children(char *name) {
-    WebTree *content = malloc(sizeof(WebTree));
-    *content = (WebTree){0};
-    da(WebTreeChild) children = da_last(webtrees)->children;
-    da_append(children, ((WebTreeChild){ .name = name, content = content }))
-    da_append(webtrees, content);
+    da_last(current_paths) = realloc(da_last(current_paths), strlen(da_last(current_paths)) + strlen(name) + 2);
+    strcat(da_last(current_paths), name);
+    strcat(da_last(current_paths), "/");
 }
 
 void end_children() {
-    size_t size = da_size(webtrees);
-    da_remove(webtrees, size);
+    size_t new_len = 1;
+    size_t next_new_len = 1;
+
+    for (size_t i = 0; da_last(current_paths)[i]; i++) {
+        if (da_last(current_paths)[i] == '/') {
+            new_len = next_new_len;
+            next_new_len = i + 1;
+        }
+    }
+
+
+    da_last(current_paths) = realloc(da_last(current_paths), new_len + 1);
+    da_last(current_paths)[new_len] = '\0';
 }
 
 void add_child(char *name, HTML (*page)()) {
@@ -128,28 +135,30 @@ void add_child(char *name, HTML (*page)()) {
     } end_children();
 }
 
-WebTree end_webtree() {
-    WebTree result = *da_last(webtrees);
-    free(da_last(webtrees));
-    da_remove(webtrees, da_size(webtrees) - 1);
+PageList end_page_list() {
+    PageList result = da_last(page_lists);
+    da_remove(page_lists, da_size(page_lists) - 1);
+    free(da_last(current_paths));
+    da_remove(current_paths, da_size(current_paths) - 1);
     return result;
 }
 
 int main(int argc, char *argv[]) {
-    begin_webtree(); {
+    begin_page_list(); {
         add_index(page_home);
         add_child("a", page_a);
-        printf("%zu", da_size(webtrees));
         add_children("b"); {
-            printf("%zu", da_size(webtrees));
             add_index(page_b);
-            printf("%zu", da_size(webtrees));
             add_child("c", page_c);
-            printf("%zu", da_size(webtrees));
         } end_children();
-        printf("%zu", da_size(webtrees));
-    } WebTree webtree = end_webtree();
-    printf("%zu", da_size(webtrees));
+    } PageList page_list = end_page_list();
+
+    for (size_t i = 0; i < da_size(page_list); i++) {
+        printf("%s\n", page_list[i].path);
+        printf("-------------------------\n");
+        printf("%s\n", page_list[i].generator());
+        printf("-------------------------\n");
+    }
 
     return EXIT_SUCCESS;
 }
